c52b4cdd287b0438191b93beeb943950a7e07174
Afaq Ahmed <afaaaq85@gmail.com>
2023-09-30 22:58:35 +0500

    basic structure for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Main from './pages/Main/Main';
 
 function App() {
   return (
     <div>
-      
+      <Main/>
     </div>
   );
 }
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    <div className='main-page'>
+      <p>Main</p>
+    </div>
+  )
+}
+
+export default Main
