38f62a24e5c244939c52cf92fa503505d5846f97
Afaq Ahmed <afaaaq85@gmail.com>
2023-10-02 22:26:13 +0500

    header section initialized

---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <a href='#aaa' className='menu'></a>
+      <div className='userItems'>
+        <a href='#ab' className='icon'></a>
+        <a href='#ab' className='icon'></a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
