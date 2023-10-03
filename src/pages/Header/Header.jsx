3108b7fd4d2a0301c1d6436344e8a76f8ba6e6cb
Afaq Ahmed <afaaaq85@gmail.com>
2023-10-03 19:24:31 +0500

    updated

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -4,10 +4,18 @@ import './Header.css'
 const Header = () => {
   return (
     <header>
-      <a href='#aaa' className='menu'></a>
+      <a href='#aaa' className='menu'>
+        <i class="bi bi-sliders"></i>
+      </a>
       <div className='userItems'>
-        <a href='#ab' className='icon'></a>
-        <a href='#ab' className='icon'></a>
+        <a href='#ab' className='icon'>
+          <i class="bi bi-heart-fill"></i>
+          <span className='like'>0</span>
+        </a>
+        <a href='#ab' className='icon'>
+          <i class="bi bi-bag-fill"></i>
+          <span className='bag'>0</span>
+        </a>
       </div>
     </header>
   )
